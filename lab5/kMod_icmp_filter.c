5137622e5014f60ee6639102815f531ccbff205d
shangzewen <792906029@qq.com>
2021-03-05 18:16:32 +0800

    added_icmp_filter

---
+++ lab5/kMod_icmp_filter.c
@@ -0,0 +1,52 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/init.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+
+static struct nf_hook_ops firewallHook;
+
+unsigned int icmpFilter(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
+{
+    struct iphdr *iph;
+    struct tcphdr *tcph;
+    iph = ip_hdr(skb);
+    tcph = (void *)iph + iph->ihl * 4;
+
+    // hardcode filter logic
+    if (iph->protocol == IPPROTO_ICMP)
+    {
+        printk(KERN_INFO "Dropping icmp packet from %d.%d.%d.%d\n", ((unsigned char *)&iph->daddr)[0], ((unsigned char *)&iph->daddr)[1], ((unsigned char *)&iph->daddr)[2], ((unsigned char *)&iph->daddr)[3]);
+        return NF_DROP;
+    }
+    else
+    {
+        return NF_ACCEPT;
+    }
+}
+
+int setUpFilter(void)
+{
+    printk(KERN_INFO "Registering a ICMP filter. \n");
+    firewallHook.hook = icmpFilter;
+    // use the netfilter hook
+    firewallHook.hooknum = NF_INET_LOCAL_IN;
+    firewallHook.pf = PF_INET;
+    firewallHook.priority = NF_IP_PRI_FIRST;
+
+    // register the hook
+    nf_register_hook(&firewallHook);
+    return 0;
+}
+void removeFilter(void)
+{
+    printk(KERN_INFO "Icmp filter has been removed. \n");
+    nf_unregister_hook(&firewallHook);
+}
+
+module_init(setUpFilter);
+module_exit(removeFilter);
+
+MODULE_LICENSE("GPL");
